6caf7521b1a7233afc5d255e5440ee512b340fb7
Monica Sharma <monicasharma1053@gmail.com>
2013-05-09 17:36:59 -0700

    personal site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonicaSite::Application.config.secret_token = 'c6d004f00a0d414bd0b17c3b763c7c540cee4caa72f7eacabbb20a8f9447eac05595b95defbb9cf62354e71816cfd8ef63827453854002580e9207cec5154c6a'
